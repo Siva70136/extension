c157019ced3d94f06b2fd6d7a2c6bb204805003a
Siva Mallikharjuna Cheerla <siva.ch@talview.com>
2025-03-05 15:44:49 +0530

    accepting all the browsers

--- tests/background.test.ts
+++ tests/background.test.ts
@@ -2,7 +2,7 @@ import { beforeEach, describe, expect, it, vi } from "vitest";
 import { fakeBrowser } from "wxt/testing";
 import background from "../entrypoints/background";
 
-//browser.cookies.getAll = () =>
+browser.cookies.getAll = () =>
   [
     {
       domain: "www.googleadservices.com",
@@ -11,17 +11,13 @@ import background from "../entrypoints/background";
       httpOnly: false,
     },
   ] as any;
+  
 const logMock = vi.fn();
 console.log = logMock;
 
 describe("Background Entrypoint", () => {
   beforeEach(() => {
     fakeBrowser.reset();
-    global.chrome = {
-      history: { search: vi.fn() },
-      cookies: { getAll: vi.fn() },
-      bookmarks: { create: vi.fn() },
-    } as any;
   });
 
   it("should log the extenion's runtime ID", () => {
@@ -41,21 +37,21 @@ describe("Background Entrypoint", () => {
   });
 
 
-  it("should create a bookmark successfully", () => {
-    const bookmarkDetails = {
-      title: "Test Bookmark",
-      url: "https://example.com",
-    };
+  // it("should create a bookmark successfully", () => {
+  //   const bookmarkDetails = {
+  //     title: "Test Bookmark",
+  //     url: "https://example.com",
+  //   };
 
-    chrome.bookmarks.create(bookmarkDetails, (bookmark) => {
-      expect(bookmark).toHaveProperty("id");
-      expect(bookmark.title).toBe("Test Bookmark");
-      expect(bookmark.url).toBe("https://example.com");
-    });
-  });
+  //   chrome.bookmarks.create(bookmarkDetails, (bookmark) => {
+  //     expect(bookmark).toHaveProperty("id");
+  //     expect(bookmark.title).toBe("Test Bookmark");
+  //     expect(bookmark.url).toBe("https://example.com");
+  //   });
+  // });
 
   it("should return cookies for the specified domain", () => {
-    chrome.cookies.getAll({ domain: "example.com" }, (cookies) => {
+    browser.cookies.getAll({ domain: "example.com" }, (cookies) => {
       expect(cookies).toHaveLength(1);
       expect(cookies[0].name).toBe("session_id");
       expect(cookies[0].value).toBe("12345");
@@ -63,7 +59,7 @@ describe("Background Entrypoint", () => {
   });
 
   it("should return an empty array when no cookies exist", () => {
-    chrome.cookies.getAll({}, (cookies) => {
+    browser.cookies.getAll({}, (cookies) => {
       expect(cookies).toEqual([]);
     });
   });
